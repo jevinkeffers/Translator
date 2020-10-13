ef0d3c96bd576d68807dfed34fb9c86c9fcf9f28
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-13 14:57:19 -0400

    Translating text to ROT13 with React

---
+++ src/components/translator.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import rot13 from "rot-thirteen";
+
+class Translator extends Component {
+    state = {
+        text: this.props.initialText || "",
+        translatedText: "",
+    };
+
+    _handleChange = (newText) => {
+        this.setState({
+        text: newText,
+        });
+    };
+
+    _handleClick = () => {
+        const { text } = this.state;
+        const translated = rot13(text);
+
+        this.setState({
+        translatedText: translated,
+        });
+    };
+
+    render() {
+        const { text, translatedText } = this.state;
+
+        return (
+        <>
+            <h1>TRANSLATOR!!</h1>
+            <form>
+            <label>
+                Text to be Translated
+                <p> <input
+                    type="text"
+                    onChange={(event) => this._handleChange(event.target.value)}
+                    />
+                </p>
+            </label>
+            <button type="button" onClick={this._handleClick}>Translate</button>
+            </form>
+            <p>Plain Text: {text}</p>
+            {!!translatedText ? <p>ROT13 Text: {translatedText}</p> : null}
+        </>
+        );
+    }
+}
+
+export default Translator;
